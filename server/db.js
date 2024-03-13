d3c76ef2ac8373e813ed34c2dd3d7cb6c66f81aa
ericpkatz <ericpkatz@gmail.com>
2024-03-13 11:45:56 -0400

    data layer is done

--- server/db.js
+++ server/db.js
@@ -1,4 +1,5 @@
 const pg = require('pg');
+const uuid = require('uuid');
 
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_talent_gh_db');
 
@@ -19,13 +20,93 @@ const createTables = async()=> {
     CREATE TABLE user_skills(
       id UUID PRIMARY KEY,
       user_id UUID REFERENCES users(id) NOT NULL,
-      skill_id UUID REFERENCES skills(id) NOT NULL
+      skill_id UUID REFERENCES skills(id) NOT NULL,
+      CONSTRAINT user_skill_unique UNIQUE (user_id, skill_id)
     );
   `;
   await client.query(SQL);
 };
 
+const createUser = async({ username, password}) => {
+  const SQL = `
+    INSERT INTO users(id, username, password)
+    VALUES($1, $2, $3)
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), username, password])
+  return response.rows[0];
+}
+
+const createUserSkill = async({ user_id, skill_id}) => {
+  const SQL = `
+    INSERT INTO user_skills(id, user_id, skill_id)
+    VALUES($1, $2, $3)
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), user_id, skill_id])
+  return response.rows[0];
+}
+
+const createSkill = async({ name }) => {
+  const SQL = `
+    INSERT INTO skills(id, name)
+    VALUES($1, $2)
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), name])
+  return response.rows[0];
+}
+
+const fetchUsers = async() => {
+  const SQL = `
+    SELECT *
+    FROM users
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+}
+
+const fetchSkills = async() => {
+  const SQL = `
+    SELECT *
+    FROM skills
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+}
+
+const fetchUserSkills = async(user_id) => {
+  const SQL = `
+    SELECT *
+    FROM user_skills 
+    WHERE user_id = $1
+  `;
+  const response = await client.query(SQL, [ user_id ]);
+  return response.rows;
+}
+
+const destroyUserSkill = async({ user_id, id}) => {
+  const SQL = `
+    DELETE FROM user_skills
+    WHERE id = $1 AND user_id = $2
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [ id, user_id ]);
+  if(!response.rows.length){
+    const error = Error('no user skill found');
+    error.status = 500;
+    throw error;
+  }
+}
+
 module.exports = {
   client,
-  createTables
+  createTables,
+  createUser,
+  fetchUsers,
+  createSkill,
+  createUserSkill,
+  fetchSkills,
+  fetchUserSkills,
+  destroyUserSkill
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
-const { client, createTables } = require('./db');
+const {
+  client,
+  createTables,
+  createUser,
+  createSkill,
+  createUserSkill,
+  fetchUsers,
+  fetchSkills,
+  fetchUserSkills,
+  destroyUserSkill
+} = require('./db');
 
 const init = async()=> {
   console.log('connecting to database');
@@ -6,6 +16,29 @@ const init = async()=> {
   console.log('connected to database');
   await createTables();
   console.log('tables created.');
+  const [moe, lucy, ethyl, juggling, spinning, yodeling, bartending] = await Promise.all([
+    createUser({ username: 'moe', password: '123'}),
+    createUser({ username: 'lucy', password: '123'}),
+    createUser({ username: 'ethyl', password: '1234'}),
+    createSkill({ name: 'juggling'}),
+    createSkill({ name: 'spinning'}),
+    createSkill({ name: 'yodeling'}),
+    createSkill({ name: 'bartending'})
+  ]);
+  console.log(await fetchUsers());
+  console.log(await fetchSkills());
+
+  const [lucyYodels, lucyBartends] = await Promise.all([
+    createUserSkill({ user_id: lucy.id, skill_id: yodeling.id}),
+    createUserSkill({ user_id: lucy.id, skill_id: bartending.id}),
+  ]);
+  console.log(await fetchUserSkills(lucy.id));
+
+  //test that this errors! moe does not yodel!
+  //await destroyUserSkill({ user_id: moe.id, id: lucyYodels.id });
+  await destroyUserSkill(lucyBartends);
+
+  console.log(await fetchUserSkills(lucy.id));
 
 };
 
